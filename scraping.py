6c4cf0888b8ba9ab102e7a297181a40add55d096
Paris (Work) <pariswork@Pariss-MacBook-Pro.local>
2024-02-28 10:35:42 -0800

    slight changes to scraping file

--- scraping.py
+++ scraping.py
@@ -5,18 +5,18 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import string
 
+
 def getPlayer():
     player = ""
 
-    while (player == ""):
-         
+    while player == "":
         player = input("Give me a player: ").strip()
 
         playerToSplit = player.split()
-        if (len(playerToSplit) < 2):
+        if len(playerToSplit) < 2:
             print("Need a first and last name!")
             player = ""
-    
+
     print()
 
     playerToSplit = player.split()
@@ -25,32 +25,34 @@ def getPlayer():
 
     urlSetter(firstName, lastName)
 
+
 def urlSetter(firstName, lastName):
-    #URL to scrape
+    # URL to scrape
     playerFullName = "{} {}".format(firstName, lastName)
     lastInitial = lastName.lower()[0]
 
     url = "https://www.basketball-reference.com/players/{}/".format(lastInitial)
 
-    # collect HTML data 
+    # collect HTML data
     html = urlopen(url)
 
-    #create beautiful soup object from HTML
+    # create beautiful soup object from HTML
     soup = BeautifulSoup(html, features="lxml")
-    
+
     # use getText() to extract the headers into a list
 
-    headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
+    headers = [th.getText() for th in soup.findAll("tr", limit=2)[0].findAll("th")]
 
-    #print(headers)
+    # print(headers)
 
     # get rows from table
-    rows = soup.findAll('tr')[1:]
-    #print(rows)
-    rows_data = [[td.getText() for td in rows[i].findAll('th')]
-                 for i in range(len(rows))]
+    rows = soup.findAll("tr")[1:]
+    # print(rows)
+    rows_data = [
+        [td.getText() for td in rows[i].findAll("th")] for i in range(len(rows))
+    ]
 
-    #rows_data.pop(20)
+    # rows_data.pop(20)
     print()
 
     strippedRows = []
@@ -58,31 +60,39 @@ def urlSetter(firstName, lastName):
     foundPlayer = False
 
     for elem in rows_data:
-        strippedRows.extend(elem[0].strip().split(','))
+        strippedRows.extend(elem[0].strip().split(","))
 
-    playerFullName = playerFullName.translate(str.maketrans('','', string.punctuation))
+    playerFullName = playerFullName.translate(str.maketrans("", "", string.punctuation))
 
     for i in range(len(strippedRows)):
-        if (strippedRows[i].translate(str.maketrans('', '', string.punctuation)).casefold() == playerFullName.casefold()):
+        if (
+            strippedRows[i]
+            .translate(str.maketrans("", "", string.punctuation))
+            .casefold()
+            == playerFullName.casefold()
+        ):
             foundPlayer = True
 
-    if (foundPlayer):
+    if foundPlayer:
         print("Found the player we wanted")
         print()
-        if (len(lastName) >= 5):
+        if len(lastName) >= 5:
             lastNameFirstFive = lastName[:5]
         else:
             lastNameFirstFive = lastName
-        if (len(firstName) >= 2):
+        if len(firstName) >= 2:
             firstNameFirstTwo = firstName[:2]
         else:
             firstNameFirstTwo = firstName
-        playerNameUrl = "{}{}01".format(lastNameFirstFive.lower(), firstNameFirstTwo.lower())
+        playerNameUrl = "{}{}01".format(
+            lastNameFirstFive.lower(), firstNameFirstTwo.lower()
+        )
         playerPage(playerNameUrl, url)
 
     else:
         print("Player wasn't found")
         getPlayer()
+
 
 def playerPage(playerNameUrl, url):
     # do something
@@ -91,6 +101,9 @@ def playerPage(playerNameUrl, url):
     html = urlopen(url)
 
     soup = BeautifulSoup(html, features="lxml")
+
+    # print(soup)
+
 
 if __name__ == "__main__":
     getPlayer()
